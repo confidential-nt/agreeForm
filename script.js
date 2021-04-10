c180612c2348b4dd9db653e465b1d01aaa94213a
confidential-nt <youyoy689@gmail.com>
2021-04-11 07:00:41 +0900

    finish

---
+++ script.js
@@ -0,0 +1,54 @@
+const allChecker = document.querySelector("#all-check");
+const checkboxes = document.querySelectorAll("[type=checkbox]");
+
+const targetCheckBoxContainer = document.querySelector(".checkbox-container");
+const targetCheckboxes = targetCheckBoxContainer.querySelectorAll(
+  "[type=checkbox]"
+);
+
+function confirmingCheck() {
+  const essentialChecker = document.querySelectorAll("[data-state=essential]");
+  const submitBtn = document.querySelector("[type=submit]");
+
+  const condition = [...essentialChecker].every((box) => box.checked);
+
+  if (condition) {
+    submitBtn.removeAttribute("disabled");
+    return;
+  }
+
+  submitBtn.setAttribute("disabled", "");
+}
+
+function LoopingForAllCheck() {
+  targetCheckboxes.forEach((checkbox) => {
+    if (allChecker.checked) {
+      checkbox.checked = true;
+      return;
+    }
+
+    checkbox.checked = false;
+  });
+}
+
+function matchmakingCheck(e) {
+  const {
+    target: {
+      dataset: { state },
+    },
+  } = e;
+
+  if (state === "all") LoopingForAllCheck();
+
+  confirmingCheck();
+}
+
+function setEventListener() {
+  checkboxes.forEach((box) => box.addEventListener("click", matchmakingCheck));
+}
+
+function main() {
+  setEventListener();
+}
+
+main();
